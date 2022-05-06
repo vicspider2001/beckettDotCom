631abdd248be157f118952f73132085280ac20ce
vicspider2001 <anogwihvictor@gmail.com>
2022-05-06 01:46:36 +0100

    beckettSite

---
+++ precache-manifest.d8bb729c0e9ac4e9211a944f87d7ebb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbf454956cf53200fe8895e14c2e474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28dc9aacecd5f72f7bf5",
+    "url": "/static/css/2.7f2699be.chunk.css"
+  },
+  {
+    "revision": "3507364e8fdb664fa2e6",
+    "url": "/static/css/main.0874e23c.chunk.css"
+  },
+  {
+    "revision": "28dc9aacecd5f72f7bf5",
+    "url": "/static/js/2.065da764.chunk.js"
+  },
+  {
+    "revision": "3507364e8fdb664fa2e6",
+    "url": "/static/js/main.125a0705.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
